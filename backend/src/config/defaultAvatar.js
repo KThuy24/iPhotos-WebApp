65875ccd03c3acde2d320290608705aae4dc1c65
KThuy24 <KThuy24>
2025-05-27 01:46:50 +0700

    Upload code lần 2

--- backend/src/config/defaultAvatar.js
+++ backend/src/config/defaultAvatar.js
@@ -1,3 +1,4 @@
+// cài đặt ảnh đại diện mặc định
 const defaultAvatar = {
     admin: "https://res.cloudinary.com/dy4h1eso2/image/upload/v1743261406/hwtv9e01yzueiopd1sdj.png",
     user: "https://res.cloudinary.com/dy4h1eso2/image/upload/v1744314095/x1t2kwtdfo1jueloo0d1.png",
